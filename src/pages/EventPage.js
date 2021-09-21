29c11fc3ace22cbf244a02dd32fc00ff6e9546e0
Vilho Voutilainen <vilho.voutilainen@metropolia.fi>
2021-09-21 19:47:11 +0300

    events.json moved to public folder for easy access on server

--- src/pages/EventPage.js
+++ src/pages/EventPage.js
@@ -1,17 +1,37 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import { useParams } from 'react-router-dom'
 import {Video} from '../components/Video/Video';
 
 
-import eventData from "../data/events.json"
 const EventPage = () => {
+
+    const [eventData, setEventData] = useState()
+    useEffect(() => {
+        console.log("useeffect")
+        fetch('data/events.json'
+            ,{
+                headers : {
+                    'Content-Type': 'application/json',
+                    'Accept': 'application/json'
+                }
+            }
+        )
+            .then((response) => {
+                console.log(response)
+                 return response.json();
+            })
+            .then((myJson) => {
+                console.log(myJson);
+                setEventData(myJson);
+            });
+    }, [])
+
     const {id} = useParams();
 
-    const eventResult = eventData.events.find(({ videoUrl }) =>
+    const eventResult = eventData?.events.find(({ videoUrl }) =>
         videoUrl === id
     )
 
-    console.log("videurl?", eventResult)
 
     if(!eventResult){
         return (<div>Event {id} not found</div>)
@@ -21,9 +41,12 @@ const EventPage = () => {
         <div>
             Tapahtuman nimi: {eventResult.name}<br/>
             Tapahtuman aika: {eventResult.startTime}<br/>
-            <Video url={eventResult.streamUrl} type={eventResult.streamVideoType}/>
+            <Video
+                url={eventResult.streamUrl}
+                type={eventResult.streamVideoType}
+            />
         </div>
     )
-}
+};
 
 export {EventPage}

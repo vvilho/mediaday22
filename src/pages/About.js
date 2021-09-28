7a7ecc545906ca383bbaa5c9335c684b04e89655
Vilho Voutilainen <vilho.voutilainen@metropolia.fi>
2021-09-28 14:00:57 +0300

    datefix

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,13 +1,24 @@
 import React from 'react';
 import {Hello} from "../components/Hello/Hello";
+import {Grid} from "@mui/material";
 
 
 const About = () => {
 
 return (
     <div>
-        <h1>About text</h1>
-        <Hello/>
+        <Grid
+            container
+            justifyContent={"center"}
+        >
+            <Grid
+                xs={6}
+                >
+                <h1>Speaker calendar</h1>
+                <Hello/>
+            </Grid>
+        </Grid>
+
     </div>
 )
 }

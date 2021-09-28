c0d207f5c240a16891a7a67c0a0ec9bcf4a83591
35840 <osku1993@luukku.com>
2021-09-28 12:52:45 +0300

    jotain

--- src/components/Video/Video.js
+++ src/components/Video/Video.js
@@ -2,6 +2,15 @@ import React, { useEffect, useRef } from "react";
 
 import videojs from "video.js";
 import "video.js/dist/video-js.css";
+import '@videojs/themes/dist/city/index.css';
+
+// Fantasy
+import '@videojs/themes/dist/fantasy/index.css';
+
+// Forest
+import '@videojs/themes/dist/forest/index.css';
+
+import '@videojs/themes/dist/sea/index.css';
 
 const Video = ({ url, type }) => {
     // return <div>Video tulee tähän, videon url: {url}</div>;
@@ -14,7 +23,6 @@ const Video = ({ url, type }) => {
         autoplay: true,
         controls: true,
         responsive: true,
-        fluid: true,
         sources: [
             {
                 src: url,
@@ -66,7 +74,7 @@ const Video = ({ url, type }) => {
 
     return (
         <div data-vjs-player>
-            <video ref={videoRef} className="video-js vjs-big-play-centered" />
+            <video ref={videoRef} className="video-js vjs-big-play-centered vjs-theme-forest" />
         </div>
     );
 };
--- src/pages/EventPage.js
+++ src/pages/EventPage.js
@@ -1,52 +1,54 @@
 import React, {useEffect, useState} from 'react';
-import { useParams } from 'react-router-dom'
+import {useParams} from 'react-router-dom';
 import {Video} from '../components/Video/Video';
-
+import {Grid} from '@mui/material';
 
 const EventPage = () => {
 
-    const [eventData, setEventData] = useState()
-    useEffect(() => {
-        console.log("useeffect")
-        fetch('data/events.json'
-            ,{
-                headers : {
-                    'Content-Type': 'application/json',
-                    'Accept': 'application/json'
-                }
-            }
-        )
-            .then((response) => {
-                console.log(response)
-                 return response.json();
-            })
-            .then((myJson) => {
-                console.log(myJson);
-                setEventData(myJson);
-            });
-    }, [])
+  const [eventData, setEventData] = useState();
+  useEffect(() => {
+    console.log('useeffect');
+    fetch('data/events.json'
+        , {
+          headers: {
+            'Content-Type': 'application/json',
+            'Accept': 'application/json',
+          },
+        },
+    ).then((response) => {
+      console.log(response);
+      return response.json();
+    }).then((myJson) => {
+      console.log(myJson);
+      setEventData(myJson);
+    });
+  }, []);
 
-    const {id} = useParams();
+  const {id} = useParams();
 
-    const eventResult = eventData?.events.find(({ videoUrl }) =>
-        videoUrl === id
-    )
+  const eventResult = eventData?.events.find(({videoUrl}) =>
+      videoUrl === id,
+  );
 
+  if (!eventResult) {
+    return (<div>Event {id} not found</div>);
+  }
 
-    if(!eventResult){
-        return (<div>Event {id} not found</div>)
-    }
-
-    return (
-        <div>
-            Tapahtuman nimi: {eventResult.name}<br/>
-            Tapahtuman aika: {eventResult.startTime}<br/>
+  return (
+      <div>
+        Tapahtuman nimi: {eventResult.name}<br/>
+        Tapahtuman aika: {eventResult.startTime}<br/>
+        <Grid container justifyContent='center' spacing={2}>
+          <Grid item xs={12} md={6}>
             <Video
                 url={eventResult.streamUrl}
                 type={eventResult.streamVideoType}
             />
-        </div>
-    )
+          </Grid>
+        </Grid>
+
+      </div>
+  );
 };
 
-export {EventPage}
+export {EventPage};

34f0bd63e645806f61695115b20c8711c4a50fa3
Joonas <joonas.lehtoranta@live.fi>
2021-09-29 09:07:15 +0300

    footer ja speakereitä

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import {Container, Typography} from '@mui/material';
+import './footer.css';
 
 const Footer = () => {
   return (
-      <Container>
+      <Container className='footer-container'>
         <Typography variant='h5'>Hei, olen footer. Whassup?</Typography>
       </Container>
   )
--- src/components/SpeakerGrid/SpeakerGrid.js
+++ src/components/SpeakerGrid/SpeakerGrid.js
@@ -1,20 +1,47 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import {Grid, Typography} from '@mui/material';
 import {Speaker} from '../Speaker/Speaker';
 
 //TODO JSON tarvitsee alla pyydetyt puhujien tiedot
-const SpeakerGrid = ({speakers}) => {
+const SpeakerGrid = () => {
+  const [speakers, setSpeakers] = useState([]);
+
+  useEffect(() => {
+    const getSpeakers = async () => {
+      try {
+      const result = await fetch('data/events.json', {
+        headers: {
+          'Content-Type': 'application/json',
+          'Accept': 'application/json',
+        },
+      });
+      console.log('getSpeakers result',await result.json());
+      const json = await result.json();
+      return await json.events;
+      } catch(err) {
+        console.log('getSpeakers error', err);
+      }
+    };
+    setSpeakers(getSpeakers);
+  }, [speakers]);
+
   return (
       <>
         <Typography variant={'h2'}>Puhujat</Typography>
-      <Grid container justifyContent='center' spacing={0}>
-        {speakers.map((speaker) =>
-          <Speaker key={speaker.id}
-                   speakerName={speaker?.speakerName ? speaker.speakerName : 'Keanu'}
-                   speakerTitle={speaker?.speakerTitle ? speaker.speakerTitle : 'Best boy'}
-                   speakerImage={speaker?.speakerImage ? speaker.speakerImage : 'https:placekeanu.com/400/300'}/>
-        )}
-      </Grid>
+        <Grid container justifyContent="center" spacing={0}>
+          {speakers?.map((speaker) =>
+              <Speaker key={speakers.findIndex(x => x.name === speaker.name)}
+                       speakerName={speaker?.name ?
+                           speaker.name :
+                           'Keanu'}
+                       speakerTitle={speaker?.company ?
+                           speaker.company :
+                           'Best boy'}
+                       speakerImage={speaker?.speakerImage ?
+                           speaker.speakerImage :
+                           'https:placekeanu.com/400/300'}/>,
+          )}
+        </Grid>
       </>
   );
 };

97eac5d7e4a535cf024005f35933f2a5203082c3
Orlian <55744999+Orlian@users.noreply.github.com>
2021-10-04 23:03:54 +0300

    Kaunistelua

--- src/components/Introduction/Introduction.js
+++ src/components/Introduction/Introduction.js
@@ -1,32 +1,36 @@
 import React from 'react';
-import {Typography} from "@mui/material";
+import {Container, Divider, Typography} from "@mui/material";
 
 const Introduction = () => {
     return (
-        <>
-            <Typography variant={'h1'} my='1rem'>
+        <Container disableGutters={true} maxWidth={false}>
+            <Typography variant={'h1'} marginY='1rem'>
                 MEDIADAY 2022
             </Typography>
-            <Typography variant={'h5'} my='1rem'>
+            <Typography variant={'h5'} marginY='1.5rem'>
                 Metropolia ammattikorkeakoulu järjestää Mediaday-tapahtuman pitkän
                 tauon jälkeen keväällä 2022.
             </Typography>
-            <Typography variant={'h5'} my='1rem'>
+            <Divider/>
+            <Typography variant={'h5'} marginY='1rem'>
                 <strong>Puhujia eri teollisuudenaloilta</strong><br/>
                 Huipputason ammattilaisten puheenvuoroja, mukana monta rautaista
                 puhujaa monelta mediakentän osa-alueelta.
             </Typography>
-            <Typography variant={'h5'} my='1rem'>
+            <Divider/>
+            <Typography variant={'h5'} marginY='1rem'>
                 <strong>Mediaday yhdistää</strong><br/>
                 Mediaday tuo opiskelijat ja yritysmaailman yhteen.
                 Näet uusimmat jutut ja tutustut ammattilaisten maailmaan.
             </Typography>
-            <Typography variant={'h5'} my='1rem'>
+            <Divider/>
+            <Typography variant={'h5'} marginY='1rem'>
                 <strong>Rento ilmapiiri</strong><br/>
                 Hyväntuulinen tekemisen meininki.
                 Meillä voi vapaasti kysellä ja ihmetellä, pääkohderyhmänä ovat opiskelijat.
             </Typography>
-        </>
+            <Divider/>
+        </Container>
     )
 }
 

1a1f9b92d8dc043a46dd4690306d518f46441106
Joonas <joonas.lehtoranta@live.fi>
2021-10-06 17:01:25 +0300

    Contact formia ja navbaria

--- src/components/menubar/DrawerComponent.js
+++ src/components/menubar/DrawerComponent.js
@@ -1,53 +1,61 @@
-import React, { useState } from "react";
+import React, {useState} from 'react';
 import {
-    Drawer,
-    IconButton,
-    List,
-    ListItem,
-    ListItemText,
-    makeStyles
-} from "@material-ui/core";
-import { Link } from "react-router-dom";
-import { Menu as MenuIcon } from "@material-ui/icons";
+  Drawer,
+  IconButton,
+  List,
+  ListItem,
+  ListItemText,
+  makeStyles,
+} from '@material-ui/core';
+import {Link} from 'react-router-dom';
+import {Menu as MenuIcon} from '@material-ui/icons';
+import {linear, Scrollchor} from 'react-scrollchor';
 
-const useStyles = makeStyles(()=>({
-    link:{
-        textDecoration:"none",
-        color: "blue",
-        fontSize: "20px",
-    },
-    icon:{
-        color: "white"
-    }
+const useStyles = makeStyles(() => ({
+  link: {
+    textDecoration: 'none',
+    color: 'blue',
+    fontSize: '20px',
+  },
+  icon: {
+    color: 'white',
+  },
 }));
 
 function DrawerComponent() {
-    const classes = useStyles();
-    const [openDrawer, setOpenDrawer] = useState(false);
-    return (
-        <>
-            <Drawer
-                open={openDrawer}
-                onClose={() => setOpenDrawer(false)}
-            >
-                <List>
-                    <ListItem onClick={() => setOpenDrawer(false)}>
-                        <ListItemText>
-                            <Link to="/aikataulu">Aikataulu</Link>
-                        </ListItemText>
-                    </ListItem>
-                    <ListItem onClick={() => setOpenDrawer(false)}>
-                        <ListItemText>
-                            <Link to="/yhteystietolomake">yhteystietolomake</Link>
-                        </ListItemText>
-                    </ListItem>
+  //const classes = useStyles();
+  const [openDrawer, setOpenDrawer] = useState(false);
+  return (
+      <>
+        <Drawer
+            open={openDrawer}
+            onClose={() => setOpenDrawer(false)}
+        >
+          <List>
+            <ListItem onClick={() => setOpenDrawer(false)}>
+              <ListItemText>
+                <Link to="/aikataulu">Aikataulu</Link>
+              </ListItemText>
+            </ListItem>
+            <ListItem onClick={() => setOpenDrawer(false)}>
+              <ListItemText>
+                <Link variant={'button'} to="/yhteystietolomake">yhteystietolomake</Link>
+              </ListItemText>
+            </ListItem>
+            <ListItem onClick={() => setOpenDrawer(false)}>
+              <Scrollchor to="#footerAbout"
+                          animate={{duration: 500, easing: linear}}>
+                Lisätietoa
+              </Scrollchor>
+            </ListItem>
 
-                </List>
-            </Drawer>
-            <IconButton onClick={() => setOpenDrawer(!openDrawer)}>
-                <MenuIcon />
-            </IconButton>
-        </>
-    );
+          </List>
+        </Drawer>
+        <IconButton onClick={() => setOpenDrawer(!openDrawer)}>
+          <MenuIcon/>
+        </IconButton>
+      </>
+  );
 }
+
 export default DrawerComponent;
--- src/pages/ContactPage.js
+++ src/pages/ContactPage.js
@@ -9,23 +9,42 @@ const ContactPage = () => {
 
   const handleSubmit = (evt) => {
     evt.preventDefault();
-    const value = document.getElementById('email').value;
+    const value = {
+      email: document.getElementById('email').value,
+      name: document.getElementById('name').value,
+      message: document.getElementById('message').value,
+    };
     console.log('Contact form submit value', value);
-    alert('Tämä on sähköposti hoho!');
   };
+
+  const handleChange = (evt) => {
+    evt.preventDefault();
+    const msgLength = document.getElementById('message').value.length;
+    console.log('smg length', msgLength);
+  }
 
   return (
       <Box sx={{
+        '& .MuiTextField-root': {m: 1},
         margin: '1rem 1rem 5rem 1rem',
         border: '1px solid grey',
         borderRadius: '5px',
         padding: '1rem',
       }} component="form" onSubmit={handleSubmit}>
-        <Typography variant="body1">Jätä meille sähköpostisi, niin otamme
-          yhteyttä.</Typography>
-        <TextField label="Email" id="email" type="email" fullWidth={true}
+        <Typography variant="body1">Ota meihin yhteyttä täältä.</Typography>
+        <TextField label="Nimesi" id="name" type="text" fullWidth={false}
                    variant="outlined" margin="dense" required={true}/>
-        <Button variant="contained" sx={{marginTop: '1rem'}} type='submit'>Lähetä</Button>
+        <TextField label="Email" id="email" type="email" fullWidth={false}
+                   variant="outlined" margin="dense" required={true}/>
+        <br/>
+        <TextField multiline={true} label="Viestisi" id="message" type="text"
+                   variant="outlined" minRows={3} required={true}
+                   fullWidth={true} inputProps={{maxLength: 1500}}
+                   helperText={`/1500`}
+        onChange={handleChange}/>
+        <br/>
+        <Button variant="contained" sx={{marginTop: '1rem'}}
+                type="submit">Lähetä</Button>
       </Box>
   );
 };

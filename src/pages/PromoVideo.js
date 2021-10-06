1a66f822d809857bb2bd2b58b7369c1b1c4b5f17
Joonas <joonas.lehtoranta@live.fi>
2021-10-06 10:09:59 +0300

    Update PromoVideo.js

--- src/pages/PromoVideo.js
+++ src/pages/PromoVideo.js
@@ -1,18 +1,26 @@
 import React, {useEffect} from 'react';
-
+import {Button, Container, Divider, FormGroup, TextField} from '@mui/material';
 
 const PromoVideo = () => {
-    useEffect(() => {
-        document.title = 'MediaDay Promo'
-    }, []);
+  useEffect(() => {
+    document.title = 'MediaDay Yhteystietolomake';
+  }, []);
 
-    return (
+  return (
+      <>
         <div>
-            <iframe id="ls_embed_1632898919"
-                    src="https://livestream.com/accounts/11707815/events/4299364/player?width=640&height=360&enableInfoAndActivity=true&defaultDrawer=&autoPlay=true&mute=false"
-                    width="640" height="360" frameBorder="0" scrolling="no" allowFullScreen autoPlay={true}></iframe>
+          <iframe id="ls_embed_1632898919"
+                  src="https://livestream.com/accounts/11707815/events/4299364/player?width=640&height=360&enableInfoAndActivity=true&defaultDrawer=&autoPlay=true&mute=false"
+                  width="640" height="360" frameBorder="0" scrolling="no"
+                  allowFullScreen autoPlay={true}></iframe>
         </div>
-    )
+        <Divider sx={{marginTop: '1rem'}}>Yhteistietolomake</Divider>
+        <FormGroup sx={{marginY: '1rem'}}>
+          <TextField label='Email' type='email' ></TextField>
+          <Button>Lähetä</Button>
+        </FormGroup>
+      </>
+  );
 };
 
-export {PromoVideo}
+export {PromoVideo};
